43e620bdfc021fb3f8f6ff243caf276de5bdbd84
Rasha-mubarak <rashamubarak458@gmail.com>
2026-02-06 13:39:41 +0530

    update api base url for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://water-montoring-system-backend-1.onrender.comgi",
 });
 
 // Attach token automatically
